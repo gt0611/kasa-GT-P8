be3811e616594ae753c9e43717da858470a0333f
Guendy Thelusmon <Guendythelusmon@gmail.com>
2024-04-19 08:06:02 +0200

    make a footer component and style for that

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import LOGOlogokasa from "../../assets/LOGOlogokasa.png";
+const Footer = () => {
+  return (
+    <footer className="footer-kasa">
+      <img src={LOGOlogokasa} alt="logo Kasa" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;

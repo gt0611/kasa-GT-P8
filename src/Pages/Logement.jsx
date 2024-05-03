d1287dce180c0be172fc46e2cf317369dd1b2b9c
Guendy Thelusmon <Guendythelusmon@gmail.com>
2024-05-03 11:38:49 +0200

    import the component tag, host and collaspe to the page logement

--- src/Pages/Logement.jsx
+++ src/Pages/Logement.jsx
@@ -3,12 +3,15 @@ import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import { useNavigate, useParams } from "react-router-dom";
 import Carousel from "../components/carousel/Carousel";
+import Tag from "../components/tag/Tag";
+import Collaspe from "../components/collaspe/Collaspe";
+import Host from "../components/host/Host";
 
 const Logement = () => {
   const [logement, setLogement] = useState();
   const { id } = useParams();
   const navigate = useNavigate();
-  const tags = logement.tags;
+  const arrayStars = [1, 2, 3, 4, 5];
 
   useEffect(() => {
     /*fonction asynchrome pour récupérer le fichier logement.json*/
@@ -27,24 +30,57 @@ const Logement = () => {
   }, [id, navigate]);
 
   return (
-    <div>
+    <div className="page-logement">
       <Header />
       {logement && (
-        <main>
+        <main className="page-logement-main">
           <Carousel pictures={logement.pictures} />
           <div className="logements">
             <div className="logements-title">
               <h1>{logement.title}</h1>
               <p>{logement.location}</p>
             </div>
-            <div className="logements-description">
-              {tags.map((tag, index) => {
-                return (
-                  <button key={(tag, index)}>
-                    <p>{tag}</p>
-                  </button>
-                );
-              })}
+            <div className="logements-info">
+              <div className="logements-info-tag">
+                {logement.tags.map((tag, index) => (
+                  <Tag tag={tag} key={tag + index} />
+                ))}
+              </div>
+              <div className="logement-info-host">
+                <Host
+                  name={logement.host.name}
+                  picture={logement.host.picture}
+                />
+                <div className="logements-stars-rating">
+                  {arrayStars.map((star) => {
+                    const nbEtoiles = parseInt(logement.rating);
+                    return (
+                      <span
+                        key={"stars" + star}
+                        className={
+                          star <= nbEtoiles
+                            ? "logements-stars-rating_1"
+                            : "logements-stars-rating_2"
+                        }
+                      >
+                        ★
+                      </span>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+            <div className="logements-collaspe">
+              <Collaspe title="Description">
+                <p>{logement.description}</p>
+              </Collaspe>
+              <Collaspe title={"Equipements"}>
+                <ul>
+                  {logement.equipments.map((equipment, index) => (
+                    <li key={equipment + index}>{equipment}</li>
+                  ))}
+                </ul>
+              </Collaspe>
             </div>
           </div>
         </main>
